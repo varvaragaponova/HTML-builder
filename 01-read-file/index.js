492ab6eb7738e1a18f0bc4963204973d3acf5d9a
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-01-22 19:02:17 +0400

    fix: change solution for task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.resolve(__dirname, 'text.txt'), (error, data) => {
-  if (error) console.error(error);
-  else console.log(data.toString());
+// fs.readFile(path.resolve(__dirname, 'text.txt'), (error, data) => {
+//   if (error) console.error(error);
+//   else console.log(data.toString());
+// });
+
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+readStream.on('data', (text) => {
+  console.log(text.toString());
+});
+
+readStream.on('error', (error) => {
+  console.log('error:', error);
+});
+
+readStream.on('end', () => {
+  readStream.close();
 });
