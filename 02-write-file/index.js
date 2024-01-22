46b23ba42804a8e716e7e34caf3feab2bb6e46ef
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-01-22 20:23:15 +0400

    feat: add solution for task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface(process.stdin);
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+console.log('Write text:');
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Program finished');
+    rl.close();
+    return;
+  }
+
+  writeStream.write(input);
+});
+
+process.on('SIGINT', () => {
+  console.log('Program finished');
+  rl.close();
+});
