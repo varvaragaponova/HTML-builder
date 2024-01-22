23e399c66f8544c925877f1e57c1efaef3dc2bb4
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-01-22 23:15:10 +0400

    feat: add solution for task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeStream = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+);
+
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    for (let file of files) {
+      if (!file.isFile() || file.name.split('.')[1] !== 'css') {
+        return;
+      }
+
+      const readStream = fs.createReadStream(
+        path.join(__dirname, 'styles', file.name),
+      );
+
+      readStream.on('data', (chunk) => {
+        writeStream.write(chunk.toString() + '\n');
+      });
+    }
+  },
+);
